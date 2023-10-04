c5361e346e79990823c000fd8bb569e188a9cde7
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-04 16:09:14 +0800

    Create komentar.dart

---
+++ komentar.dart
@@ -0,0 +1,12 @@
+/// ini adalah documentation
+/// yang biasa akan digenerate menjadi dart doc
+void main() {
+  // This is variable
+  var name = 'Noor Habibah';
+  
+/**
+ * ini adalah komentar
+ * lebih dari satu baris
+ */
+ print(name);
+}
