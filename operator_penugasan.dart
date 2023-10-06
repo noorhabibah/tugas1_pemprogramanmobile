02d5a5aa1ceb1daabd94bc23f5c72899fdec2c4e
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-06 10:52:27 +0800

    Create operator_penugasan.dart

---
+++ operator_penugasan.dart
@@ -0,0 +1,15 @@
+void main(){
+  var a = 25;
+  // a = a + 25;
+  a += 10;
+  print(a);
+
+  var i = 0;
+  // i++; // i = i + 1;
+  var j = i++; // j = i, i++
+  var k = ++i; // k = (++i)
+
+  print(i);
+  print(j);
+
+}
