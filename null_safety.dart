2cb2c15f826ce88adbdd9f2eb1290978dcb8cabf
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-06 20:56:09 +0800

    Create null_safety.dart

---
+++ null_safety.dart
@@ -0,0 +1,43 @@
+void main(){
+  int? age = null;
+  age = 1;
+
+  if(age != null){
+    double ageDouble = age.toDouble();
+    print(ageDouble);
+  }
+
+  String name = 'Bibah';
+  String? nullableName = name;
+
+  int? nullablePrice = null;
+  if(nullablePrice != null){
+    int price = nullablePrice;
+  }
+
+  String? guest;
+  //  guest = 'Bibah';
+  String guestName = guest ?? 'Guest';
+  // String guestName =  guest != null ? guest : 'Guest';
+
+  // if(guest != null){
+  //   guestName = guest;
+  // }else{
+  //   guestName = 'Guest';
+  // }
+
+  print(guestName);
+
+  int? nullableNumber;
+  // nullableNumber = 10;
+  // int nonNullableNumber = nullableNumber!; // error
+
+  int? dataInt;
+  double? dataDouble = dataInt?.toDouble();
+
+  // if(dataInt != null){
+  //   dataDouble = dataInt.toDouble();
+  // }
+
+  print(dataDouble);
+}
