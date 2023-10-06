5cb5404424a6f486fc8e3101941fedecafae0bcb
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-06 20:44:02 +0800

    Create switch_case.dart

---
+++ switch_case.dart
@@ -0,0 +1,18 @@
+void main(){ 
+  var nilai = 'B';
+  switch(nilai){
+    case 'A':
+      print('Anda Lulus Dengan Baik');
+      break;
+    case 'B':
+      print('Anda Lulus Dengan Cukup Baik');
+    case 'C':
+      print('Anda Lulus');
+      break;
+    case 'D':
+      print('Anda Tidak Lulus');
+      break;
+    default:
+      print('Mungkin Anda Salah Jurusan');
+  }  
+}
