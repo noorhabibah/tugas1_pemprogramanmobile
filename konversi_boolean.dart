cf90d95c2c8fa2e60eb2f03f1c5dcb77e98303ce
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-06 11:22:01 +0800

    Create konversi_boolean.dart

---
+++ konversi_boolean.dart
@@ -0,0 +1,10 @@
+void main(){
+  var inputString = 'false';
+  var inputBoolean = inputString == 'true';
+
+  var StringFromBoolean = inputBoolean.toString();
+
+  print(inputBoolean);
+  print(StringFromBoolean);
+
+}
