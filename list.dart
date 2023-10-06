8205014e8bbd395a1cae0a6b91ed12bb2181e685
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-06 11:10:46 +0800

    Update list.dart

--- list.dart
+++ list.dart
@@ -1,10 +1,32 @@
-void main() {  
- var name = <String>[];
-  
- name.add('Noor');
- name.add('Habibah');
- name.add('Kim');
-  
- print(name);
- print(name.length);
+void main(){
+
+  List<int> listInt = [];
+
+  var listString = <String>[];
+
+  print(listInt);
+  print(listString);
+
+  var names = <String>[
+    'Noor',
+    'Habibah',
+    'Kim',
+  ];
+
+  // names.add('Noor');
+  // names.add('Habibah');
+  // names.add('Kim');
+
+  print(names);
+  print(names.length);
+
+  print(names[0]);
+
+  names[0] = 'Bibah';
+  print(names[0]);
+
+  names.removeAt(1);
+  print(names);
+  print(names[1]);
+
 }
