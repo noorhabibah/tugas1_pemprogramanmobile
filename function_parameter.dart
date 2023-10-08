86532f3bcf101ea8ba088fe90f2dfae2f4a7eca1
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-08 16:40:26 +0800

    Create function_parameter.dart

---
+++ function_parameter.dart
@@ -0,0 +1,8 @@
+void main(){
+  void sayHello(String firstName, String lastName){
+    print('Hello $firstName $lastName');
+  }
+  
+  sayHello('Noor', 'Habibah');
+  sayHello('Kim', 'Mingyu');
+  }
