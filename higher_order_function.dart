c57dd84af42fa594fff68036c9e4f4e70d9c4694
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-09 11:14:11 +0800

    Create higher_order_function.dart

---
+++ higher_order_function.dart
@@ -0,0 +1,13 @@
+void main(){
+  String filterBadWord(String name){
+    if(name == 'gila'){
+      return'****';
+    } else{
+      return name;
+    }
+  }
+  
+  //sayHello('Bibah', filterBadWord);
+  //sayHello('gila', filterBadWord);
+  print('Bibah' 'gila');
+}
