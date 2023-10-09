eaff855cf50d51abd5273f1945811204d29109b7
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-09 11:38:05 +0800

    Update and rename konversi.dart to konversi_string.dart

---
+++ konversi_string.dart
@@ -0,0 +1,12 @@
+void main(){
+  var inputString = '1000';
+  var inputInt = int.parse(inputString);
+  var inputDouble = double.parse(inputString);
+ 
+  var doubleFromInt = inputInt.toDouble();
+  var intFromDouble = inputDouble.toInt();
+  
+
+  var StringFromInt = inputInt.toString();
+  var StringFromDouble = inputDouble.toString();
+}
